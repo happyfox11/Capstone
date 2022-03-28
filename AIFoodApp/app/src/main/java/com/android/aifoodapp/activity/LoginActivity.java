848a8fbd28ac6064f410cb3c399772aa342cb8ac
happyfox11 <hanstar0211@kyonggi.ac.kr>
2022-03-28 15:57:02 +0900

    앱 첫 화면 로그인 페이지 UI 디자인

---
+++ AIFoodApp/app/src/main/java/com/android/aifoodapp/activity/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.android.aifoodapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import com.android.aifoodapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Activity activity;
+    LinearLayout btn_kakao, btn_google, btn_email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+        initialize();
+        addListener();
+    }
+
+    //변수 초기화
+    private void initialize(){
+        activity = this;
+        btn_kakao = findViewById(R.id.btn_kakao);
+        btn_google = findViewById(R.id.btn_google);
+        btn_email = findViewById(R.id.btn_email);
+    }
+
+    //리스너 생성
+    private void addListener(){
+        btn_kakao.setOnClickListener(listener_kakao_sign);
+        btn_google.setOnClickListener(listener_google_sign);
+        btn_email.setOnClickListener(listener_email_sign);
+    }
+
+    //1. 카카오 로그인 리스너 등록
+    private final View.OnClickListener listener_kakao_sign = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(activity, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    //2. 구글 로그인 리스너 등록
+    private final View.OnClickListener listener_google_sign = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(activity, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    //3. 이메일 로그인 리스너 등록
+    private final View.OnClickListener listener_email_sign = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(activity, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+}
