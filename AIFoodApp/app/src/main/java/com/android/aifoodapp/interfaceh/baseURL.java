08631674dc7ff9904ff9ec41facfa108895fe2db
psy <kdmsws@gmail.com>
2022-05-07 17:07:22 +0900

    주간통계 2,4번째 프래그먼트 색상 변경 추가

--- AIFoodApp/app/src/main/java/com/android/aifoodapp/interfaceh/baseURL.java
+++ AIFoodApp/app/src/main/java/com/android/aifoodapp/interfaceh/baseURL.java
@@ -3,8 +3,8 @@ package com.android.aifoodapp.interfaceh;
 public interface baseURL {
     //핸드폰이용하는 경우 -- https://rateye.tistory.com/1082?category=1026651
     //String url = "http://192.168.219.101:8080"; //juhee
-    //String url = "http://172.30.1.29:8080"; //soyoung
-    String url = "http://192.168.0.6:8080"; //jisoo
+    String url = "http://172.30.1.29:8080"; //soyoung
+   // String url = "http://192.168.0.6:8080"; //jisoo
     //String url = "http://192.168.35.2:8080"; //hanbyul
     //avd를 이용하는 경우
     //String url = "http://10.0.2.2:8080";
